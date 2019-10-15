4f2a76e478d5893346ca8a9eaa3dbced561dbdfc
vonaxl <vonaxlcorso@gmail.com>
2019-10-15 17:00:39 +0200

    well what can i say? I'M THE JAVASCRIPT MONSTER

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+function canzone(){
+  document.getElementById('showme').style.opacity = "1";
+  document.getElementById('musicme').style.opacity = "1";
+}
+function stop(){
+  document.getElementById('showme').style.opacity = "0";
+  document.getElementById('musicme').style.opacity = "0";
+}
